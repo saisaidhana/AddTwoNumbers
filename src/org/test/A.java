7dce304c1638071479e7540cb12a6d4a025f3afe
saisaidhana <dhanaseelanr1996@gmail.com>
2022-04-22 15:43:06 +0530

    new coded added for abstraction

---
+++ src/org/test/A.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class A {
+	
+	public int add(int a, int b) {
+		int c = a+b;
+		return c;
+		
+	}
+
+}
---
+++ src/org/test/B.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class B extends A {
+	
+	public static void main(String[] args) {
+		A a = new A();
+	int add = a.add(25,25);
+	
+	
+
+if(add==50) {
+	System.out.println("valid");
+	
+}
+else {
+	System.out.println("not valid");
+}
+}
+}
